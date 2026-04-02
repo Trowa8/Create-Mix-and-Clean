9799b54f825fbd63ba421fe6a654a4571914ec05
Trowa8 <likepcandtv@gmail.com>
2026-04-02 20:38:49 +0300

    2.0.0b

--- src/main/java/net/mcreator/createmixandclean/init/CreateMixAndCleanModBlocks.java
+++ src/main/java/net/mcreator/createmixandclean/init/CreateMixAndCleanModBlocks.java
@@ -1,6 +1,6 @@
 /*
- *    MCreator note: This file will be REGENERATED on each build.
- */
+*    MCreator note: This file will be REGENERATED on each build.
+*/
 package net.mcreator.createmixandclean.init;
 
 import net.minecraftforge.registries.RegistryObject;
@@ -16,25 +16,21 @@ import net.mcreator.createmixandclean.block.ChlorineGasBlock;
 import net.mcreator.createmixandclean.CreateMixAndCleanMod;
 
 public class CreateMixAndCleanModBlocks {
-	public static final DeferredRegister<Block> REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, CreateMixAndCleanMod.MODID);
-	public static final RegistryObject<Block> CHLORINE_GAS;
-	public static final RegistryObject<Block> HYDROGEN_GAS;
-	public static final RegistryObject<Block> HYDROCHLORIC_ACID;
-	public static final RegistryObject<Block> ELECTROLYZER;
-	static {
-		CHLORINE_GAS = REGISTRY.register("chlorine_gas", ChlorineGasBlock::new);
-		HYDROGEN_GAS = REGISTRY.register("hydrogen_gas", HydrogenGasBlock::new);
-		HYDROCHLORIC_ACID = REGISTRY.register("hydrochloric_acid", HydrochloricAcidBlock::new);
-	// Start of user code block custom blocks
-		ELECTROLYZER = REGISTRY.register("electrolyzer", () ->
-    	new ElectrolyzerBlock(
-       		net.minecraft.world.level.block.state.BlockBehaviour.Properties.of()
-            	.strength(3.5f, 6f)
-            	.requiresCorrectToolForDrops()
-            	.sound(net.minecraft.world.level.block.SoundType.METAL)
-    	)
-		);
-	// End of user code block custom blocks
-	}
-
+public static final DeferredRegister<Block> REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, CreateMixAndCleanMod.MODID);
+public static final RegistryObject<Block> CHLORINE_GAS;
+public static final RegistryObject<Block> HYDROGEN_GAS;
+public static final RegistryObject<Block> HYDROCHLORIC_ACID;
+public static final RegistryObject<Block> ELECTROLYZER;
+static {
+CHLORINE_GAS =
+REGISTRY.register("chlorine_gas", ChlorineGasBlock::new);
+HYDROGEN_GAS =
+REGISTRY.register("hydrogen_gas", HydrogenGasBlock::new);
+HYDROCHLORIC_ACID =
+REGISTRY.register("hydrochloric_acid", HydrochloricAcidBlock::new);
+ELECTROLYZER =
+REGISTRY.register("electrolyzer", ElectrolyzerBlock::new);
+}
+// Start of user code block custom blocks
+// End of user code block custom blocks
 }
